c11b4562c97ff5cb2232ecfd1b274fd579f175d2
Atique <nsuatique@gmail.com>
2024-06-03 08:02:38 +0600

    backup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { DefaultButton } from '@fluentui/react/lib/Button';
+
 
 function App() {
+
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to Atique.
         </p>
         <a
           className="App-link"
@@ -17,6 +21,8 @@ function App() {
         >
           Learn React
         </a>
+
+    
       </header>
     </div>
   );
